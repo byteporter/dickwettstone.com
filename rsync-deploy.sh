032489d1131de20d33b433c1b0e6fe888826f868
James <james@byteporter.com>
2018-08-28 16:43:44 -0500

    Changed deployment URL added google search console ownership verification file to static

--- rsync-deploy.sh
+++ rsync-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync $@ -avzhe ssh --progress --exclude '.env' --exclude 'docker-compose.override.yml' --exclude '.git*' --exclude 'rsync-deploy.sh' . core@jameslind.info:/opt/dickwettstone_com
+rsync $@ -avzhe ssh --progress --exclude '.env' --exclude 'docker-compose.override.yml' --exclude '.git*' --exclude 'rsync-deploy.sh' . core@richardwettstone.com:/opt/dickwettstone_com
