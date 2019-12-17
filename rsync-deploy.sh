ff65ad6342f5f9e7c25dffb5e64b3c8f127d2bbe
James <james@byteporter.com>
2019-12-17 16:32:35 -0600

    Removed dickwettstone.com config because that domain is no longer active. Cleaned up other files.

--- rsync-deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-rsync "$@" -avzhe ssh --progress --exclude '.env' --exclude 'docker-compose.override.yml' --exclude '.git*' --exclude 'rsync-deploy.sh' . core@richardwettstone.com:/opt/dickwettstone_com
